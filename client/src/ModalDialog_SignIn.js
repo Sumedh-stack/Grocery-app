ecc6a464342eb5a5bb8422d9658cf4b2d6e0b952
Adarsh Mall <adarshmall1000@gmail.com>
2021-09-30 01:29:17 +0530

    Fix React Integration with backend for google authentication

--- client/src/ModalDialog_SignIn.js
+++ client/src/ModalDialog_SignIn.js
@@ -2,23 +2,38 @@ import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import Box from '@material-ui/core/Box'
 import SignIn from './SignIn';
+import axios from 'axios';
 import {GoogleLogin,GoogleLogout} from 'react-google-login';
-const ModalDialog_SignIn = ({ open, handleClose }) => {
+
+const ModalDialog_SignIn = ({ open, handleClose, baseUrl }) => {
+  const responseSuccessGoogle = (response) => {
+      console.log(response);
+      axios({
+        method : "POST",
+        url : `${baseUrl}/api/auth/googlelogin`,
+        data: {tokenId: response.tokenId}
+      }).then(res => {
+        console.log("Google Login Success" ,res);
+      }).catch(err => console.log(err));
+  }
+  const responseFailureGoogle = (response) => {
+    console.log("Google Login Failed : ", response);
+  }
   return (
     // props received from App.js
     
       <Dialog open={open} onClose={handleClose}>
       <SignIn handleClose={handleClose} />
       <div className="google" style={{paddingBottom: "30px"}}>
-      
       <Box textAlign='center'>
+      
       <h3>OR</h3>
          <GoogleLogin  
-                  clientId="64634358040-0c803m7f7d39g9dmqt8hmhmbmudppfcb.apps.googleusercontent.com"
+                  clientId="997283847932-6kqihc3fgfcvrk6nk7af4cnq2p0h3crk.apps.googleusercontent.com"
               
                 theme='dark'
-              onSuccess={response=>{console.log(response.profileObj)}}
-              onFailure={console.log("fail")}
+              onSuccess={responseSuccessGoogle}
+              onFailure={responseFailureGoogle}
               cookiePolicy={'single_host_origin'}
                 /> 
       </Box>
