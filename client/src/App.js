da6ff78bb18852c9c34247f6c02ac7e63f586431
unknown <rajputsumedhsingh@gmail.com>
2021-10-01 17:35:18 +0530

    Integrate Landing Page with React

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import SignInComp from './SignInComp';
 import SignUpComp from './SignUpComp';
-
+import Home from './components/Home';
 // const GoogleLogoutButton = props => (
 //   <GoogleLogout
 //    clientId={"64634358040-0c803m7f7d39g9dmqt8hmhmbmudppfcb.apps.googleusercontent.com"}
@@ -12,15 +12,7 @@ import SignUpComp from './SignUpComp';
 //  )
 
 function App() {
-
-  return (
-    // <div className="App">
-    <div className="App">
-       <SignInComp /> 
-        <SignUpComp/>
-       
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
--- client/src/SignInComp.js
+++ client/src/SignInComp.js
@@ -1,31 +1,30 @@
-import React,{useState} from 'react';
-import {GoogleLogin,GoogleLogout} from 'react-google-login';
+import React, { useState } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import ModalDialog from './ModalDialog';
 import Button from '@material-ui/core/Button';
 import { baseUrl } from './baseUrl';
 import ModalDialog_SignIn from './ModalDialog_SignIn';
 
-
-
-
-
-function SignInComp(){
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => {
-        setOpen(true);
-      };
-      const handleClose = () => {
-        setOpen(false);
-      };
-    return(
-        <div className="sign-in">
-        <Button variant="contained" color="primary" onClick={handleOpen}>
+function SignInComp() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className="sign-in">
+      <Button variant="contained" color="primary" onClick={handleOpen}>
         SignIn
       </Button>
-      <ModalDialog_SignIn open={open} handleClose={handleClose} baseUrl={baseUrl}/> 
-        </div>
-    )
+      <ModalDialog_SignIn
+        open={open}
+        handleClose={handleClose}
+        baseUrl={baseUrl}
+      />
+    </div>
+  );
 }
-
 
 export default SignInComp;
